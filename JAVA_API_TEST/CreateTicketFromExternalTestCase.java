92ff5189ceb8d2c1a450cbb2347da272d825ceba
miksovskymartindev <miksovskymartindev@gmail.com>
2019-10-31 13:28:06 +0100

    cleaning

--- JAVA_API_TEST/CreateTicketFromExternalTestCase.java
+++ JAVA_API_TEST/CreateTicketFromExternalTestCase.java
@@ -1,9 +1,6 @@
 package de.usu.test.web.suite.testcase.interfaces;
 
-import java.util.ArrayList;
 import java.util.Date;
-import java.util.List;
-
 import org.openqa.selenium.Keys;
 import de.usu.test.web.api.module.component.Toolbar.Perspective;
 import de.usu.test.web.core.WebTestCase;
@@ -15,19 +12,16 @@ public class CreateTicketFromExternalTestCase extends WebTestCase {
 	public static final String ENDPOINT = "http://localhost:8080/vmweb/services/api/execwf";
 	private Response response;
 	GIHandler genericHandler;
-	List<Object> testList = new ArrayList<Object>();
+	private static final String NAME = "dummy";
+	private static final String SURENAME = "dummy;
 
 	public void testRun() {
-		
-		String name = "dummy";
-		String sureName = "dummy";
-		
 		
 		genericHandler = new GIHandler();
 		genericHandler.onCreatePerson().onWrapper().setService("InterfaceTransactionStart");
 		genericHandler.onCreatePerson().onPersonBody().setInterfaceActionName("VMEx_VM_CreatePerson_Basic");		
-		genericHandler.onCreatePerson().onPersonBody().setJlastname(sureName);
-		genericHandler.onCreatePerson().onPersonBody().setJfirstname(name);
+		genericHandler.onCreatePerson().onPersonBody().setJlastname(SURENAME);
+		genericHandler.onCreatePerson().onPersonBody().setJfirstname(NAME);
 		
 		response = genericHandler.sendRequestAndGetResponse(
 			genericHandler.onCreatePerson().getCreatePersonJsoned(),
@@ -44,7 +38,7 @@ public class CreateTicketFromExternalTestCase extends WebTestCase {
 		onToolbar().switchToBusinessView("End User");
 		onTileList().findAndClickTile("Persons (All)");
 
-		onCatalog().onCatalogPanel().findQuickSearchInput().sendKeys(sureName+Keys.ENTER);
-		assertTrue(onCatalog().onCatalogPanel().isRecordPresent("lastname", sureName));
+		onCatalog().onCatalogPanel().findQuickSearchInput().sendKeys(SURENAME + Keys.ENTER);
+		assertTrue(onCatalog().onCatalogPanel().isRecordPresent("lastname", SURENAME));
 	}
 }
